3ee7ec98d51a74d442111e3267edb35f87c19ce0
Oleksandra221 <studia.anokhina@gmail.com>
2023-06-13 14:56:03 +0200

    feat: new methods 2

---
+++ src/main/java/com/uep/wap/controller/SignUpExceptions.java
@@ -0,0 +1,7 @@
+package com.uep.wap.controller;
+
+public class SignUpExceptions extends Exception {
+    public SignUpExceptions(String errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ src/main/java/com/uep/wap/service/IUserProfileService.java
@@ -0,0 +1,14 @@
+package com.uep.wap.service;
+
+import com.uep.wap.dto.UserProfileDTO;
+import com.uep.wap.model.UserProfile;
+
+import java.util.List;
+
+public interface IUserProfileService {
+    public void addUser(UserProfileDTO userProfileDTO);
+    public List<UserProfile> getAllUsers();
+
+    public UserProfile findUserByEmail(String email);
+    public void deleteUserByEmail(String email);
+}
